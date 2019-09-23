6aeb87c7fce2bb980a71fb43425ebdd34f6e3a21
Rick Ratzel <rratzel@nvidia.com>
2019-09-23 16:54:06 -0700

    Added command to build the package, since --output command does not.

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ export HOME=$WORKSPACE
 
 source activate gdf
 
+conda build -c conda-forge ./recipe/libcypher-parser
+
 conda build -c conda-forge ./recipe/libcypher-parser --output | xargs \
      anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label thirdparty --force
